477a249e5af7b64ca84704c1353df5dc8c05ce67
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-08-06 11:06:07 -0600

    Creating the form for new projects

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import NewProject from '../projects/NewProject';
 
 const Sidebar = () => {
 	return (  
 		<aside>
 			<h1>MERN<span>Tasks</span></h1>
+			<NewProject />
+			
 			<div className="proyectos">
 				<h2>Tus proyectos</h2>
 			</div>
