b0b164ada3b07a82359f5ac73d73ffbb3c5d0fd8
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-08-07 12:11:33 -0600

    Creating the project provider

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Login from 'components/auth/Login'
 import NewAccount from 'components/auth/NewAccount'
 import Projects from 'components/projects/Projects'
+import ProjectState from './context/projects/ProjectState'
 
 function App() {
   return (
+    <ProjectState>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/new-account" component={NewAccount} />
+          <Route exact path="/projects" component={Projects} />
+        </Switch>
+      </Router>
 
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/new-account" component={NewAccount} />
-        <Route exact path="/projects" component={Projects} />
-      </Switch>
-    </Router>
+    </ProjectState>
   )
 }
 
---
+++ src/context/projects/projectState.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-vars */
+
+import React, { useReducer } from 'react'
+import projectContext from './projectContext'
+import projectReducer from './projectReducer'
+
+const ProjectState = props => {
+
+	const initialState = {
+		form: false
+	}
+
+	// Dispatch to execute actions
+
+	const [state, dispatch] = useReducer(projectReducer, initialState)
+
+	// Function series for the CRUD
+
+	return (
+		<projectContext.Provider
+			value={{
+				form: state.form
+			}}
+		>
+			{props.children}
+		</projectContext.Provider>
+	)
+}
+
+export default ProjectState
+
