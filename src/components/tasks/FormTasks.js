a752ecb918091f626eebe8e24b914f68309273f0
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-08-08 15:03:33 -0600

    Inserting tasks into current project

--- src/components/tasks/FormTasks.js
+++ src/components/tasks/FormTasks.js
@@ -1,18 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import ProjectContext from '../../context/projects/projectContext'
+import taskContext from '../../context/tasks/taskContext';
+
+
 const FormTasks = () => {
 
 	// Extract if a project is active
 	const projectsContext = useContext(ProjectContext)
 	const { project } = projectsContext;
 
+	// Getting function from task context
+	const tasksContext = useContext(taskContext)
+	const { addTask } = tasksContext;
+
+	// State the form
+	const [task, saveTask] = useState({
+		name: ''
+	})
+	// Extract the name project
+	const { name } = task;
 	// If there is not a project selected
 
 	if (!project) return null
 
 	// Array destructuring to extract the current project
-	// const [currentProject] = project;
+	const [currentProject] = project;
 
+	// Read form values
+	const handleChange = e => {
+		saveTask({
+			...task,
+			[e.target.name]: e.target.value
+		})
+	}
 	const onSubmit = e => {
 		e.preventDefault()
 
@@ -21,7 +41,9 @@ const FormTasks = () => {
 		// Pass validation
 
 		// Add the new task to the task state
-
+		task.projectId = currentProject.id
+		task.estado = false
+		addTask(task)
 		// Restart the form
 	}
 	return (
@@ -35,6 +57,8 @@ const FormTasks = () => {
 						className="input-text"
 						placeholder="Nombre de la tarea"
 						name="name"
+						value={name}
+						onChange={handleChange}
 
 					/>
 				</div>
